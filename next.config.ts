eb187e7b3a6c62ba2bff929b30cf648627a6a85a
Mikolaj Bielinski <mikolaj.bielinski10@gmail.com>
2026-08-29 09:30:19 +0200

    feat: Return deleted modules for docker image size.

--- next.config.ts
+++ next.config.ts
@@ -2,18 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
-  images: {
-    unoptimized: true,
-  },
-  outputFileTracingExcludes: {
-    "*": [
-      "node_modules/typescript/**",
-      "node_modules/@img/**",
-      "node_modules/sharp/**",
-      "node_modules/@esbuild/**",
-      "node_modules/caniuse-lite/**",
-    ],
-  },
 };
 
 export default nextConfig;
